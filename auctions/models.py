d1fe753a4c8f13012c8cf941cec6347f37e12832
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2021-01-31 14:34:16 +0100

    reset to baseline

--- auctions/models.py
+++ auctions/models.py
@@ -3,14 +3,4 @@ from django.db import models
 
 
 class User(AbstractUser):
-	pass
-
-class Listing():
-	title = models.CharField(max_length=100)
-	description = models.CharField(max_length=2000)
-	starting_bid = models.FloatField()
-	image = models.ImageField(null=True)
-
-	def __str__(self):
-		return f"{self.title}: {self.current_bid}"
-
+    pass
